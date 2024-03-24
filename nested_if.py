dfa09fdeb2f41569ee18171b1ad06d88659b2e31
Arimlna <160694744+Arimlna@users.noreply.github.com>
2024-03-24 14:52:41 +0700

    Add files via upload

---
+++ nested_if.py
@@ -0,0 +1,13 @@
+inputan = int (input("Masukan Nilai "))
+if inputan <= 59:
+    print("nilai Skor ={inputan} = E Not Pass" )
+elif inputan <= 69:
+    print("nilai Skor ={inputan} = D Nosaticfactory" )
+elif inputan <= 79:
+    print("nilai Skor ={inputan} = C Satisfying" )
+elif inputan <= 89:
+    print("nilai Skor ={inputan} = B Verysatisfy" )
+elif inputan <= 100:
+    print("nilai Skor ={inputan} = A with Compliment" )
+else:
+    print("masukan nilai ulang")
